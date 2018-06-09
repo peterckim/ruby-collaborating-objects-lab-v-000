45a317158a7e72f7e428d14980079306d822d8b3
Peter Kim <peterckim96@gmail.com>
2018-06-09 19:23:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,27 +1,3 @@
 class Song
-  attr_accessor :name, :artist_name, :artist
-  
-  def initialize(name)
-    @name = name
-  end
-  
-  def name=(name)
-    @name = name
-  end
-  
-  def artist=(artist)
-    @artist = artist
-  end
-  
-  def artist_name=(artist_name)
-    @artist_name = artist_name
-  end
-  
-  def self.new_by_filename(file)
-    file_name = file.split(" - ")
-    new_song = Song.new(file_name[1])
-    new_song.artist = Artist.find_or_create_by_name(file_name[0])
-    new_song
-  end
-  
+
 end
