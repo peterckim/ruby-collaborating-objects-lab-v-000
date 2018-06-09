5b5f5eb5793891776c6c5d64a76b628c9fce7ff1
Peter Kim <peterckim96@gmail.com>
2018-06-09 19:33:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,13 @@
 class Song
+  def initialize(song_name)
+    @name = song_name
+  end
   
+  def name=(name)
+    @name = name
+  end
+  
+  def artist=(artist)
+    self.artist = artist
+  end
 end
