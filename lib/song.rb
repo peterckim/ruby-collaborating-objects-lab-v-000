f9efc9e8eb5ce6d63470b08bf082aa3ab6eb6823
Peter Kim <peterckim96@gmail.com>
2018-06-09 19:35:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,13 @@ class Song
   def artist=(artist)
     self.artist = artist
   end
+  
+  def self.new_by_filename(file)
+    file_array = file.split(" - ")
+    song_artist = file_array[0]
+    song_title = file_array[1]
+    new_song = Song.new(song_title)
+    new_song.artist = song_artist
+    new_song
+  end
 end
