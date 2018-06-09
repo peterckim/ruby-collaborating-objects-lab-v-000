27ef926769fd116f1f21b226bd54c980282fb572
Peter Kim <peterckim96@gmail.com>
2018-06-09 19:23:44 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,15 +1,5 @@
 class MP3Importer
-  attr_accessor :path, :size, :files
-  
-  def initialize(path)
-    @path = path
-    @files = []
-  end
-  
-  def size
-    
-  end
-  
+
   def files
     @files = Dir.glob("#{@path}/*.mp3").map do |file|
       File.basename(file) #strips the pathname
